a063a9fd79472d6d902df4ae9313a1aa737f0bc0
David Castillo <hola@davecastworks.com>
2020-05-15 15:57:29 -0400

    Put files for github pages

---
+++ precache-manifest.7a7ed846accad885cef56d0fb6171c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fae89c12feaa932baaa0d7be446a09",
+    "url": "/social-media-dashboard/index.html"
+  },
+  {
+    "revision": "dda46a009012bb2fcb8f",
+    "url": "/social-media-dashboard/static/css/main.181d1413.chunk.css"
+  },
+  {
+    "revision": "aef2a28a6cef82c24473",
+    "url": "/social-media-dashboard/static/js/2.98824f87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard/static/js/2.98824f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda46a009012bb2fcb8f",
+    "url": "/social-media-dashboard/static/js/main.83d9a6c5.chunk.js"
+  },
+  {
+    "revision": "ddc475220355821a86ee",
+    "url": "/social-media-dashboard/static/js/runtime-main.b794e3f2.js"
+  }
+]);
